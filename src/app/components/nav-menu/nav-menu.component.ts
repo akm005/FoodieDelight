a5ea6b35d015d2d6abc923f763fb06e45a5f0812
Anup Kumar Singh <88195114+akm005@users.noreply.github.com>
2024-09-09 07:37:42 +0530

    - minor bug fix mobile

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -7,5 +7,6 @@ import { Component, Input } from '@angular/core';
 })
 export class NavMenuComponent {
   @Input({required:true}) isLogin!:boolean 
+  @Input({required:true}) flexRow:boolean = true
 
 }
