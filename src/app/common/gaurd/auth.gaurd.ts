82b87ad9565b63e8e33e55e021e19e33139254f4
Anup Kumar Singh <88195114+akm005@users.noreply.github.com>
2024-09-10 00:39:08 +0530

    Fixed Type in code

--- src/app/common/gaurd/auth.gaurd.ts
+++ src/app/common/gaurd/auth.gaurd.ts
@@ -1,31 +1,36 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, ActivatedRoute } from '@angular/router';
+import {
+  CanActivate,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  ActivatedRoute,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from '../../services/login.service';
 // import { AuthService } from './auth.service'; // Replace with the actual path
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
-  constructor(private login: LoginService, private router: Router, private activatedRoute: ActivatedRoute) {
-
-  }
+  constructor(
+    private login: LoginService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot,
+    state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    console.log(this.activatedRoute.snapshot.url)
     const isAuthenticated = this.login.isAuthenticated();
 
     if (isAuthenticated) {
       return true;
     } else {
-     
-      this.login.setRedirectUrl(state.url); 
-      this.router.navigate(['/login']);
+      this.login.setRedirectUrl(state.url);
+      this.router.navigate(['admin/login']);
       return false;
     }
   }
